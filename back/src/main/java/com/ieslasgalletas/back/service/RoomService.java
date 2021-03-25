c329145334da144548b0e86bf0259cc4fe7c4dd4
victorfch <vfariachavez@gmail.com>
2021-03-25 16:02:13 +0000

    Delete back directory

--- back/src/main/java/com/ieslasgalletas/back/service/RoomService.java
+++
@@ -1,9 +0,0 @@
-package com.ieslasgalletas.back.service;
-
-import java.util.List;
-
-import com.ieslasgalletas.back.entity.Room;
-
-public interface RoomService {
-	public abstract List<Room> listAllRooms();
-}
