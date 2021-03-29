0077b5a643c7bc538f0ac29ecda66d0fe8d06441
lorena jimenez <lorenajimenez@MacBook-Pro-de-lorena.local>
2021-03-29 13:31:05 +0100

    menu navegacion interno funciona basico

---
+++ src/front/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Home</h2>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
---
+++ src/front/src/views/Usuarios.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Página de usuarios</h1>
+  </div>
+</template>
