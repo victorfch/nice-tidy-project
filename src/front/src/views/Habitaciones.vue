2ca3aecdb3fca43fe6d42e802b66629e431d07f6
LorenaDeveloper <developerlorena@yahoo.com>
2021-03-30 21:27:56 +0100

    habitaciones con formulario falta responsive

--- src/front/src/views/Habitaciones.vue
+++ src/front/src/views/Habitaciones.vue
@@ -4,21 +4,39 @@
       <div class="col-12"><h3>Habitaciones</h3></div>
     </div>
     <hr />
+    <form class="form-group row pl-3" >
+      <label for="number" class="col-form-label mr-2">Nº Habitación: </label>
+      <input type="number" class="form-control col-1" id="number" />
+      <label for="beds" class="col-form-label ml-4 mr-2">Nº Camas: </label>
+      <input type="number" class="form-control col-1" id="beds" />
+      <label for="type" class="col-form-label ml-4 mr-2">Tipo: </label>
+      <select class="form-select">
+        <option selected value="1">Individual</option>
+        <option value="2">Doble</option>
+        <option value="3">Triple</option>
+        <option value="4">Suite</option>
+      </select>
+      <button type="submit" class="btn celeste ml-4">Añadir</button>
+    </form>
     <div class="row">
       <div
-        class="col-2"
+        class="col-3 mb-3"
         v-for="habitacion in habitaciones"
         :key="habitacion.id"
       >
         <div class="card">
-          <div class="class-divider p-2">
+          <div class="class-divider p-3">
             <div class="num">Habitación {{ habitacion.number }}</div>
-            <div class="type" v-if="habitacion.type==1">Individual </div>
-            <div class="type" v-else-if="habitacion.type==2">Doble </div>
-            <div class="type" v-else-if="habitacion.type==3">Triple </div>
-            <div class="type" v-else-if="habitacion.type==4">Suite </div>
-            <div class="beds" v-if="habitacion.bedsNumber==1">{{ habitacion.bedsNumber }} cama</div>
-            <div class="beds" v-if="habitacion.bedsNumber>1">{{ habitacion.bedsNumber }} camas</div>
+            <div class="type" v-if="habitacion.type == 1">Individual</div>
+            <div class="type" v-else-if="habitacion.type == 2">Doble</div>
+            <div class="type" v-else-if="habitacion.type == 3">Triple</div>
+            <div class="type" v-else-if="habitacion.type == 4">Suite</div>
+            <div class="beds" v-if="habitacion.bedsNumber == 1">
+              {{ habitacion.bedsNumber }} cama
+            </div>
+            <div class="beds" v-if="habitacion.bedsNumber > 1">
+              {{ habitacion.bedsNumber }} camas
+            </div>
           </div>
           <div class="card-footer text-center">
             <button
@@ -56,22 +74,24 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           this.habitaciones = data;
-          this.habitaciones.sort((a,b)=>{
-            if(a.number>b.number){
+          this.habitaciones.sort((a, b) => {
+            if (a.number > b.number) {
               return 1;
-            }else{
+            } else {
               return -1;
             }
-          })
+          });
         });
     },
   },
-    mounted(){
+  mounted() {
     this.recibirHabitaciones();
-  }
+  },
 };
 </script>
 
 <style scoped>
-
+.celeste{
+  background-color: #E3F2FD;
+}
 </style>
