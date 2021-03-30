32133e38e683d87d1be1aa7aea365de7d4e78927
VictorFCh <vfariachavez@gmail.com>
2021-03-30 16:37:03 +0100

    navbar + bootstrap

--- src/front/src/App.vue
+++ src/front/src/App.vue
@@ -1,31 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/usuarios">Usuarios</router-link> |
-    <router-link to="/habitaciones">Habitaciones</router-link>
+  <div class="row">
+    <navbar />
+    <div class="container mt-3">
+      <router-view />
+    </div>
   </div>
-  <router-view/>
 </template>
-
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: { Navbar },
+  name: "App",
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/front/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="nav vh-100 flex-column navbar-light"
+    style="background-color: #e3f2fd"
+  >
+    <li class="nav-item">
+      <router-link class="nav-link" to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" to="/habitaciones"
+        >Habitaciones</router-link
+      >
+    </li>
+    <li class="nav-item">
+      <router-link class="nav-link" to="/usuarios">Usuarios</router-link>
+    </li>
+
+    <button class="btn btn-info mt-auto">Cerrar sesión</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.nav {
+  padding: 30px;
+}
+
+.nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+.nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
