091675237051981342fdd05277afbe878cf4d446
VictorFCh <vfariachavez@gmail.com>
2021-03-31 19:47:16 +0100

    add room front

--- src/front/src/views/Habitaciones.vue
+++ src/front/src/views/Habitaciones.vue
@@ -4,20 +4,37 @@
       <div class="col-12"><h1>Habitaciones</h1></div>
     </div>
     <hr />
-    <form @submit.prevent="addHab" class="form-group row pl-3 justify-content-center">
+    <div class="alert alert-danger" v-if="error">
+      Debe rellenar todos los campos
+    </div>
+    <form
+      ref="form"
+      @submit.prevent="addHab"
+      class="form-group row pl-3 justify-content-center"
+    >
       <label for="number" class="col-form-label mr-1">Nº Habitación: </label>
-      <input v-model="numero" min="1" oninput="validity.valid||(value='');" type="number" 
-            class="form-control col-5 col-sm-2 m-2" id="number"  />
+      <input
+        v-model="numero"
+        min="1"
+        type="number"
+        class="form-control col-5 col-sm-2 m-2"
+        id="number"
+      />
       <div class="d-sm-none w-100"></div>
       <label for="beds" class="col-form-label ml-2 mr-1">Nº Camas: </label>
-      <input v-model="beds" type="number" min="1" oninput="validity.valid||(value='');" 
-            class="form-control col-5 col-sm-2 m-2" id="beds"/>
+      <input
+        v-model="beds"
+        type="number"
+        min="1"
+        oninput="validity.valid||(value='');"
+        class="form-control col-5 col-sm-2 m-2"
+        id="beds"
+      />
 
       <div class="d-block d-sm-block d-lg-none w-100"></div>
 
       <label for="type" class="col-form-label ml-2 mr-1">Tipo: </label>
       <select class="form-select col-5 col-sm-2 m-2" v-model="selected">
-        <option disabled value="">Seleccione un tipo</option>
         <option value="1">Individual</option>
         <option value="2">Doble</option>
         <option value="3">Triple</option>
@@ -26,41 +43,30 @@
       <div class="d-sm-none w-100"></div>
       <button type="submit" class="btn btn-info ml-4">Añadir</button>
     </form>
-    <div class="alert alert-danger" v-if="error">Debe rellenar todos los campos</div>
-    <div class="row">
+    <div class="card-columns">
       <div
-        class="col-12 col-sm-6 col-md-4 mb-3"
+        class="card pt-2"
         v-for="habitacion in habitaciones"
         :key="habitacion.id"
       >
-        <div class="card">
-          <div class="class-divider p-3">
-            <div class="num offset-1">Habitación {{ habitacion.number }}</div>
-            <div class="type offset-1" v-if="habitacion.type == 1">Individual</div>
-            <div class="type offset-1" v-else-if="habitacion.type == 2">Doble</div>
-            <div class="type offset-1" v-else-if="habitacion.type == 3">Triple</div>
-            <div class="type offset-1" v-else-if="habitacion.type == 4">Suite</div>
-            <div class="beds offset-1" v-if="habitacion.bedsNumber == 1">
-              {{ habitacion.bedsNumber }} cama
+        <div class="class-divider">
+          <div class="ml-5">
+            <div>Habitación {{ habitacion.number }}</div>
+            <div>
+              {{ getRoomType(habitacion.type) }}
             </div>
-            <div class="beds offset-1" v-if="habitacion.bedsNumber > 1">
-              {{ habitacion.bedsNumber }} camas
+            <div>
+              {{ habitacion.bedsNumber }} cama{{
+                habitacion.bedsNumber > 1 ? "s" : null
+              }}
             </div>
           </div>
           <div class="card-footer text-center">
-            <button
-              @click="modificarHabitacion"
-              type="button"
-              class="btn btn-sm text-left mr-3"
-            >
-              <img src="../assets/edit.png" />
+            <button @click="modificarHabitacion" type="button" class="btn">
+              <i class="fas fa-pencil-alt"></i>
             </button>
-            <button
-              @click="eliminarHabitacion"
-              type="button"
-              class="btn btn-sm text-left"
-            >
-              <img src="../assets/cross.png" />
+            <button @click="eliminarHabitacion" type="button" class="btn">
+              <i class="far fa-trash-alt"></i>
             </button>
           </div>
         </div>
@@ -72,14 +78,13 @@
 <script>
 export default {
   name: "Habitaciones",
-  el: '#alerta',
   data() {
     return {
       habitaciones: [],
-      numero:'',
-      beds:'',
-      selected:'',
-      error:false,
+      numero: "",
+      beds: "",
+      selected: "1",
+      error: false,
     };
   },
   methods: {
@@ -88,7 +93,7 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           this.habitaciones = data;
-          this.habitaciones.sort((a, b) => {
+          this.habitaciones = this.habitaciones.sort((a, b) => {
             if (a.number > b.number) {
               return 1;
             } else {
@@ -97,17 +102,45 @@ export default {
           });
         });
     },
-    addHab(){
-      if(this.numero == "" || this.beds =="" || this.selected=="" ){
-        this.error=true;
-      }else{
-        this.error=false;
+    getRoomType(type) {
+      if (type == 1) {
+        return "Individual";
+      }
+      if (type == 2) {
+        return "Doble";
+      }
+      if (type == 3) {
+        return "Triple";
+      }
+      return "Suite";
+    },
+    addHab() {
+      this.error = false;
+      if (this.numero == "" || this.beds == "") {
+        this.error = true;
+      }
+      if (!this.error) {
+        const room = {
+          number: parseInt(this.numero),
+          bedsNumber: parseInt(this.beds),
+          type: parseInt(this.selected),
+        };
+        const options = {
+          method: "post",
+          body: JSON.stringify(room),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        fetch("http://localhost:8080/rooms", options)
+          .then((response) => response.json())
+          .then(() => this.getHabitaciones());
       }
     },
-    modificarHabitacion(){
+    modificarHabitacion() {
       console.log("modificando...");
     },
-    eliminarHabitacion(){
+    eliminarHabitacion() {
       console.log("eliminando...");
     },
   },
@@ -119,11 +152,8 @@ export default {
 
 <style scoped>
 /* si no lo pongo siempre.. se mueve el diseño al pasar el ratón x encima*/
-input[type=number]::-webkit-inner-spin-button {
-    opacity: 1
-}
-.card{
-  min-width: 130px;
+input[type="number"]::-webkit-inner-spin-button {
+  opacity: 1;
 }
 /* .celeste {
   background-color: #e3f2fd;
