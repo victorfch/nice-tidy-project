e21b4e7f24a1ac27d2411c29cd913bdc5a87a18f
VictorFCh <vfariachavez@gmail.com>
2021-05-04 19:53:30 +0100

    charts

--- src/front/src/views/Home.vue
+++ src/front/src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <h2>Home</h2>
+
+  <div class="container">
+    <img class="pie" src="../assets/chart1.png" alt="chart1" />
+    <img class="pie" src="../assets/chart1.png" alt="chart2" />
+    <br />
+    <img class="line" src="../assets/chart2.png" alt="chart3" />
+  </div>
 </template>
 
 <script>
@@ -7,3 +14,17 @@ export default {
   name: "Home",
 };
 </script>
+<style scoped>
+.container img:nth-child(2) {
+  float: right;
+  margin-right: 10%;
+}
+.pie {
+  height: 200px;
+  width: 200px;
+}
+.line {
+  width: 800px;
+  height: 600px;
+}
+</style>
